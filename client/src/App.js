13552b47f93e7fcc6bf4d8c1ab3be9a95a379e98
ryanzubayer <ryanzubayer95@gmail.com>
2022-06-29 10:53:18 -0400

    made changes to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,8 @@ function App() {
           <Navbar />
           <Switch>
             <Route exact path="/" component={SearchBooks} />
-            <Route exact path="/saved" component={SavedBooks} />
+            <Route exact path="/saved" component={SavedBooks} /
+            <Route exact path="/friends" component={friendlists} /
             <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
           </Switch>
         </>
