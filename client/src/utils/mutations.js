85d91e7a5aee1eadc83d248ed40168e934526034
https://uconn.bootcampcontent.com/theknightmoores-xJPRH <theknightmoores@gmail.com>
2022-06-29 10:43:47 -0400

    updated comments

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -78,3 +78,19 @@ export const REMOVE_BOOK = gql`
     }
   }
 `;
+
+
+export const ADD_COMMENT = gql`
+  mutation addComment($commentText: String!) {
+    addComment(commentText: $commentText) {
+      _id
+      commentText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
